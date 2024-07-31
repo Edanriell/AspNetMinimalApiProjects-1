3a8ee3600bd4ba18c3dfee8d7917af6c843fd6c5
Edanriell <Edanriell@gmail.com>
2024-07-31 21:23:27 +0300

    update

---
+++ MinimalWebAPIAuthenticationAndAuthorization/MinimalWebAPIAuthenticationAndAuthorization/Authorization/MaintenanceTimeRequirement.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace MinimalWebAPIAuthenticationAndAuthorization.Authorization;
+
+public class MaintenanceTimeRequirement : IAuthorizationRequirement
+{
+    public TimeOnly StartTime { get; init; }
+
+    public TimeOnly EndTime { get; init; }
+}
